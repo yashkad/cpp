eb3954dce530342990cf54e8ecbb9b129c4d5fb4
yash <yash@pop-os.localdomain>
2022-01-29 22:56:17 +0530

    commit yash

--- oop/rough.cpp
+++ oop/rough.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
-#include <string>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-int main() {
-    string s = "234";
-    cout<<s.substr(1);
-    return 0;
-}
+// int main() {
+//     string s = "234";
+//     cout<<s.substr(1);
+//     return 0;
+// }
+
+Enter number : 
+9
+ nine
+ 
+Enter number : 
+19
+ nineteen
+ 
+Enter number : 
+101
+one hundred  one
+
+Enter number : 
+119
+one hundred  nineteen
+
+Enter number : 
+0091
+ ninty one
+ 
+Enter number : 
+8011
+eight thousand  eleven
+
+Enter number : 
+9111
+nine thousand one hundred  eleven
